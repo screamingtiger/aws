b1cc3230a65cb650fd4991842a23723f36a64893
Joey Thompson <nwjt22@gmail.com>
2022-04-24 00:15:44 -0500

    ...

--- bin/aws.ts
+++ bin/aws.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { AwsStack } from '../lib/aws-stack';
+import { FreeStack } from '../lib/aws-stack';
 
 const app = new cdk.App();
-new AwsStack(app, 'AwsStack', {
+new FreeStack(app, 'FreeStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/free-stack.ts
+++ lib/free-stack.ts
@@ -1,12 +1,18 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
 
-export class AwsStack extends Stack {
+
+export class FreeStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    
+    new s3.Bucket(this, 'TrashCan', {
+      versioned: false
+    });
+
+
 
     // The code that defines your stack goes here
 
