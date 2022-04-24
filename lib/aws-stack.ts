c51d805fa665b8ca8f8ed7d42f18ecc5826a83c9
Joey Thompson <nwjt22@gmail.com>
2022-04-23 23:35:12 -0500

    ..

--- lib/aws-stack.ts
+++ lib/aws-stack.ts
@@ -6,6 +6,8 @@ export class AwsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    
+
     // The code that defines your stack goes here
 
     // example resource
