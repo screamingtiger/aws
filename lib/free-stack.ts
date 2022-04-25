f4b0bc87470e97104921cd2aa7f286cd07bf639b
Joey Thompson <nwjt22@gmail.com>
2022-04-24 20:28:43 -0500

    add usage plan and stage, api key

--- lib/free-stack.ts
+++ lib/free-stack.ts
@@ -6,18 +6,22 @@ import { aws_apigateway as apigw } from 'aws-cdk-lib';
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
+import { ApiKey } from 'aws-cdk-lib/aws-apigateway';
+import { PlacementConstraint } from 'aws-cdk-lib/aws-ecs';
 
 
 export class FreeStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    new s3.Bucket(this, 'TrashCan2', {
-      versioned: false
-    });
-
-    const trashGW : apigw.RestApi = new apigw.RestApi(this,"trashapi", {});
-    const todos = trashGW.root.addResource('health-check');
+    /*
+        new s3.Bucket(this, 'TrashCan2', {
+          versioned: false
+        });
+    */
+    const api: apigw.RestApi = new apigw.RestApi(this, "trashapi", {});
+    // const mainAPI = trashGW.root.addResource('health-check',{});
+    const v1 = api.root.addResource('health-check');
+    //const healthCheck = v1.addResource('health-check');
 
     const healthCheckLambda = new lambda.Function(this, 'health-check-lambda', {
       runtime: lambda.Runtime.NODEJS_14_X,
@@ -25,14 +29,34 @@ export class FreeStack extends Stack {
       code: lambda.Code.fromAsset(path.join(__dirname, '/../lib/lambdas/')),
     });
 
-    todos.addMethod(
-      'GET',
-      new apigw.LambdaIntegration(healthCheckLambda, {proxy: true}),
-    );
-    this.exportValue(trashGW.url,{name: "apiurl"});
-    new cdk.CfnOutput(this, 'apiUrl2', {value: trashGW.url});
-    
 
+    const key = api.addApiKey("trashKey", { apiKeyName: "trashKey", value: "joeythompsonisnumberone" });
+
+    const usagePlan = api.
+      addUsagePlan('trashUsage', {
+        name: 'trashUP',
+        throttle: {
+          rateLimit: 10,
+          burstLimit: 2
+        }
+      });
+
+    usagePlan.addApiStage({
+      stage: api.deploymentStage
+    })
+
+    v1.addMethod(
+      'GET',
+      new apigw.LambdaIntegration(healthCheckLambda, { proxy: true }),
+      { apiKeyRequired: true }
+    );
+
+    usagePlan.addApiKey(key);
+
+
+    this.exportValue(api.url, { name: "apiurl" });
+    new cdk.CfnOutput(this, 'apiUrl2', { value: api.url });
+    this.exportValue(key.keyId, { name: "trashKey" });
 
     // The code that defines your stack goes here
 
